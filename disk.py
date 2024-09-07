e8bd458ebd40bb36e13255cfb987b94913f0e44c
kristatraboulay <kristatraboulay@gmail.com>
2024-09-07 08:31:44 -0700

    wrote final_disk_speed method and two unittests

--- disk.py
+++ disk.py
@@ -1,5 +1,8 @@
 import numpy as np
+import math
+import unittest
 
+g = 9.81
 
 def final_disk_speed(height: float, length: float, incline: float, mass: float, friction: float, radius: float) -> float:
     """
@@ -13,3 +16,39 @@ def final_disk_speed(height: float, length: float, incline: float, mass: float, 
     :param radius: the radius of the disk (meters)
     :return: the speed of the disk (m/s)
     """
+
+    initial_energy = mass * g * (height + radius)
+    final_speed = math.sqrt(2 * initial_energy / mass)
+    
+    return final_speed
+
+
+class TestFinalDiskSpeed(unittest.TestCase):
+    """ Test class for function disk.final_disk_speed """
+
+    def test_final_disk_speed_ex1(self):
+        height1 = 5.0
+        length1 = 10.0
+        incline1 = 30.0
+        mass1 = 10.0
+        friction1 = 1.0
+        radius1 = 1.0
+
+        actual = final_disk_speed(height1, length1, incline1, mass1, friction1, radius1) 
+        expected = 10.850
+        self.assertEqual(round(actual, 3), expected) 
+
+    def test_final_disk_speed_ex2(self):
+        height2 = 20.0 * math.sin(math.pi / 12.0)
+        length2 = 20.0
+        incline2 = 15.0
+        mass2 = 30.0
+        friction2 = 0.0
+        radius2 = 2.0
+
+        actual = final_disk_speed(height2, length2, incline2, mass2, friction2, radius2) 
+        expected = 11.866
+        self.assertEqual(round(actual, 3), expected) 
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
